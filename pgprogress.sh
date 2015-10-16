60e8bc65ddac4d5aa466b6dc382f60b19316f465
Postgres Plus Advanced Server <postgres@dbp7m1.cbmi.wucon.wustl.edu>
2015-10-16 15:11:09 -0500

    Added in ability to glean information from processes that are reading and writing to multiple files

--- pgprogress.sh
+++ pgprogress.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Required - Enter Database Name to check against
-# DBNAME="<DATABASENAME>"
-DBNAME="postgres"
+# Enter Database Name to check against
+DBNAME="I2B2"
 
 if [ $# -eq 0 ]
   then
@@ -19,29 +18,53 @@ MFILE=`grep read proctrace.txt |awk -F',' {'print $1;'}|awk -F"(" {'print $2;'}|
 SMFILE=`echo $MFILE|xargs`
 NUMMFILE=`echo $SMFILE|wc -w`
 
+WFILES=`grep write proctrace.txt |awk -F',' {'print $1;'}|awk -F"(" {'print $2;'}|sort|uniq|xargs`
+
 if [ $NUMMFILE -gt 1 ]; then
-	echo "Sorry - This process cannot be tracked by this script at this time."
-	echo "This is is more useful for Index and Table creation"
+	echo "Process is reading multiple files and progress cannot be tracked"
+	echo "Files being READ are:"
+	echo "---------------------"
+	for RFILE in $SMFILE
+	do
+		ONFILE=`ls -l /proc/$1/fd |grep " $RFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $6;'}|awk -F"." {'print $1;'}`
+		ONSUBFILE=`ls -l /proc/$1/fd |grep " $RFILE ->"|awk -F . '{print $NF}'`
+		OIDNAME=`psql -t -d $DBNAME -c "SELECT relname from pg_class where oid = $ONFILE;"`
+		echo "Process is reading $OIDNAME at $ONSUBFILE"
+	done
+        
 	echo
-	exit
+	echo "Files being WRITTEN TO are:"
+        echo "---------------------------"
+	
+	for WFILE in $WFILES
+	do
+		ONFILE=`ls -l /proc/$1/fd |grep " $WFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $6;'}|awk -F"." {'print $1;'}`
+                ONSUBFILE=`ls -l /proc/$1/fd |grep " $WFILE ->"|awk -F . '{print $NF}'`
+                OIDNAME=`psql -t -d $DBNAME -c "SELECT relname from pg_class where oid = $ONFILE;"`
+                echo "Process is writing $OIDNAME at $ONSUBFILE"
+	done
+	echo
+else
+
+	echo "Found file being accessed as FD $MFILE"
+	ls -l /proc/$1/fd |grep " $MFILE ->"
+
+	MDIR=`ls -l /proc/$1/fd |grep " $MFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $1"/"$2"/"$3"/"$4"/"$5;'}`
+	ONSUBFILE=`ls -l /proc/$1/fd |grep " $MFILE ->"|awk -F . '{print $NF}'`
+
+	ONLYGIG=`echo $ONSUBFILE|grep -c "/"`
+
+	if [ $ONLYGIG -ne 0 ]; then
+		ONSUBFILE=0
+	fi
+
+	ONFILE=`ls -l /proc/$1/fd |grep " $MFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $6;'}|awk -F"." {'print $1;'}`
+
+	MAXSUBFILE=`ls -al $MDIR |grep $ONFILE|sort|grep -v "_vm"|grep -v "_fsm"|tail -n1|awk -F . '{print $NF}'`
+
+	OIDNAME=`psql -t -d $DBNAME -c "SELECT relname from pg_class where oid = $ONFILE;"`
+	echo "Process is reading $OIDNAME at $ONSUBFILE gigabytes of $MAXSUBFILE"
+	echo
+
 fi
 
-echo "Found file being accessed as FD $MFILE"
-ls -l /proc/$1/fd |grep " $MFILE ->"
-
-MDIR=`ls -l /proc/$1/fd |grep " $MFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $1"/"$2"/"$3"/"$4"/"$5;'}`
-ONSUBFILE=`ls -l /proc/$1/fd |grep " $MFILE ->"|awk -F . '{print $NF}'`
-
-ONLYGIG=`echo $ONSUBFILE|grep -c "/"`
-
-if [ $ONLYGIG -ne 0 ]; then
-	ONSUBFILE=0
-fi
-
-ONFILE=`ls -l /proc/$1/fd |grep " $MFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $6;'}|awk -F"." {'print $1;'}`
-
-MAXSUBFILE=`ls -al $MDIR |grep $ONFILE|sort|grep -v "_vm"|grep -v "_fsm"|tail -n1|awk -F . '{print $NF}'`
-
-OIDNAME=`psql -t -d $DBNAME -c "SELECT relname from pg_class where oid = $ONFILE;"`
-echo "Process is reading $OIDNAME at $ONSUBFILE gigabytes of $MAXSUBFILE"
-echo
