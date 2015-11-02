5c97ea1ac0aae4a6ca6f9a796c1dda73eb49daf0
Postgres Plus Advanced Server <postgres@dbp7m1.cbmi.wucon.wustl.edu>
2015-11-02 12:21:11 -0600

    Major rewrite to include functions and allow for multiple IO files

--- pgprogress.sh
+++ pgprogress.sh
@@ -1,85 +1,99 @@
 #!/bin/bash
 
 # Enter Database Name to check against
-DBNAME="postgres"
+DBNAME="I2B2"
+MPPID=$1
 
+# Error check input
 if [ $# -eq 0 ]
   then
     echo "No PID supplied"
     exit
 fi
 
-echo "Checking process $1"
+#functions
+
+#Take in ONFILE raw numeric file and output relfilname
+function getfname {
+	local ONFILE=$1
+	if [[ $ONFILE =~ .*_fsm.* ]]; then
+		ONFILE="UNKNOWN";
+	elif [[ $ONFILE =~ .*pg_xlog.* ]]; then
+		ONFILE="UNKNOWN";
+	fi
+	if [ "$ONFILE" != "UNKNOWN" ]; then
+		OIDNAME=`psql -t -d $DBNAME -c "SELECT relname from pg_class where relfilenode = $ONFILE;"`
+	fi	
+}
+
+#Build read or write lists and flag if temp file is in list
+function buildlists {
+	local FTYPE=$1
+	MFILE=`grep $FTYPE proctrace.txt |awk -F',' {'print $1;'}|awk -F"(" {'print $2;'}|sort|uniq|xargs`
+	NUMMFILE=`echo $MFILE|wc -w`
+	
+	for IOFNAME in $MFILE
+	do
+		FULLNAME=`ls -l /proc/$MPPID/fd |grep " $IOFNAME ->"`
+       		ISTMP=`echo $FULLNAME|grep -c "pgsql_tmp"`
+	done
+	if [ $FTYPE == "read" ];then
+		RFILES=$MFILE
+	elif [ $FTYPE == "write" ];then
+		WFILES=$MFILE
+	fi
+}
+
+#Print list of files being read or written
+function printlists {
+	IOTYPE=$1
+	if [ $IOTYPE == "read" ]; then
+		local IOFILES=$RFILES
+	elif [ $IOTYPE == "write" ]; then
+		local IOFILES=$WFILES
+	fi
+	#echo "DEBUG IOFILES $IOFILES RFIES $RFILES WFILES $WFILES"
+	for IOFILE in $IOFILES
+	do
+		ONFILE=`ls -l /proc/$MPPID/fd |grep " $IOFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $6;'}|awk -F"." {'print $1;'}`
+                ONSUBFILE=`ls -l /proc/$MPPID/fd |grep " $IOFILE ->"|awk -F . '{print $NF}'`
+                FULLNAME=`ls -l /proc/$MPPID/fd |grep " $IOFILE ->"|awk -F"-> " {'print $2;'}`
+                MDIR=`ls -l /proc/$MPPID/fd |grep " $IOFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $1"/"$2"/"$3"/"$4"/"$5;'}`
+		MAXSUBFILE=`ls -al $MDIR |grep $ONFILE|sort|grep -v "_vm"|grep -v "_fsm"|tail -n1|awk -F . '{print $NF}'`
+		isonlygig 
+		getfname $ONFILE
+                echo "Process is reading from $OIDNAME at $ONSUBFILE out of $MAXSUBFILE at $FULLNAME"
+	done
+}
+
+function isonlygig {
+	# Check if table is under a gig or IO is on 0GB table
+	ONLYGIG=`echo $ONSUBFILE|grep -c "/"`
+
+        if [ $ONLYGIG -ne 0 ]; then
+                ONSUBFILE=0
+        fi
+}	
+
+
+#Main
+echo "Checking process $MPPID"
 echo $(date)
 echo
 
-timeout 1 strace -o proctrace.txt -p $1 &>/dev/null
-MFILE=`grep read proctrace.txt |awk -F',' {'print $1;'}|awk -F"(" {'print $2;'}|sort|uniq`
-SMFILE=`echo $MFILE|xargs`
-NUMMFILE=`echo $SMFILE|wc -w`
+timeout 1 strace -o proctrace.txt -p $MPPID &>/dev/null
+buildlists read
+buildlists write
 
-for RFNAME in $SMFILE
-do
-	FULLNAME=`ls -l /proc/$1/fd |grep " $RFNAME ->"`
-	ISTMP=`echo $FULLNAME|grep -c "pgsql_tmp"`
-	#echo "Is temp? $ISTMP $RFNAME"
-done
 
-WFILES=`grep write proctrace.txt |awk -F',' {'print $1;'}|awk -F"(" {'print $2;'}|sort|uniq|xargs`
+echo "Files being READ are:"
+echo "---------------------"
+printlists read
+       
+echo
+echo "Files being WRITTEN TO are:"
+       echo "---------------------------"
+printlists write
 
-for WFNAME in $WFILES
-do
-        FULLNAME=`ls -l /proc/$1/fd |grep " $WFNAME ->"`
-        ISTMP=`echo $FULLNAME|grep -c "pgsql_tmp"`
-        #echo "Is temp? $ISTMP $WFNAME"
-done
-
-if [ $NUMMFILE -gt 1 ] || [ -z "$MFILE" ] || [ $ISTMP -gt 0 ]; then
-	echo "Process's progress cannot be tracked."
-	echo "Either it is reading multiple files, writing only, or working with tmp tables"
-	echo "Files being READ are:"
-	echo "---------------------"
-	for RFILE in $SMFILE
-	do
-		ONFILE=`ls -l /proc/$1/fd |grep " $RFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $6;'}|awk -F"." {'print $1;'}`
-		ONSUBFILE=`ls -l /proc/$1/fd |grep " $RFILE ->"|awk -F . '{print $NF}'`
-		FULLNAME=`ls -l /proc/$1/fd |grep " $RFILE ->"|awk -F"-> " {'print $2;'}`
-		echo "Process is reading $FULLNAME"
-	done
-        
-	echo
-	echo "Files being WRITTEN TO are:"
-        echo "---------------------------"
-	
-	for WFILE in $WFILES
-	do
-		ONFILE=`ls -l /proc/$1/fd |grep " $WFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $6;'}|awk -F"." {'print $1;'}`
-                ONSUBFILE=`ls -l /proc/$1/fd |grep " $WFILE ->"|awk -F . '{print $NF}'`
-                FULLNAME=`ls -l /proc/$1/fd |grep " $WFILE ->"|awk -F"-> " {'print $2;'}`
-		echo "Process is writing $FULLNAME"
-	done
-	echo
-else
-
-	echo "Found file being accessed as FD $MFILE"
-	ls -l /proc/$1/fd |grep " $MFILE ->"
-
-	MDIR=`ls -l /proc/$1/fd |grep " $MFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $1"/"$2"/"$3"/"$4"/"$5;'}`
-	ONSUBFILE=`ls -l /proc/$1/fd |grep " $MFILE ->"|awk -F . '{print $NF}'`
-
-	ONLYGIG=`echo $ONSUBFILE|grep -c "/"`
-
-	if [ $ONLYGIG -ne 0 ]; then
-		ONSUBFILE=0
-	fi
-
-	ONFILE=`ls -l /proc/$1/fd |grep " $MFILE ->"|awk -F"-> " {'print $2;'}|awk -F"/" {'print $6;'}|awk -F"." {'print $1;'}`
-
-	MAXSUBFILE=`ls -al $MDIR |grep $ONFILE|sort|grep -v "_vm"|grep -v "_fsm"|tail -n1|awk -F . '{print $NF}'`
-
-	OIDNAME=`psql -t -d $DBNAME -c "SELECT relname from pg_class where relfilenode = $ONFILE;"`
-	echo "Process is reading $OIDNAME at $ONSUBFILE gigabytes of $MAXSUBFILE"
-	echo
-
-fi
-
+echo
+exit
