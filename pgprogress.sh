3855097cd0f3b220714b38e03857fa51a45330f2
James Miller <jvaskonen@gmail.com>
2019-02-15 16:19:54 -0800

    Handle temp files and WAL gracefully Handle unsplit files

--- pgprogress.sh
+++ pgprogress.sh
@@ -17,15 +17,8 @@ QUERY=`psql -Atc "select query from pg_stat_activity where pid = $MPPID"`
 
 #Take in ONFILE raw numeric file and output relfilname
 function getfname {
-	local ONFILE=$1
-	if [[ $ONFILE =~ .*_fsm.* ]]; then
-		ONFILE="UNKNOWN";
-	elif [[ $ONFILE =~ .*pg_xlog.* ]]; then
-		ONFILE="UNKNOWN";
-	fi
-	if [ "$ONFILE" != "UNKNOWN" ]; then
-		OIDNAME=`psql -t -d $DBNAME -c "SELECT relname from pg_class where relfilenode = $ONFILE;"`
-	fi	
+  #echo "psql -U postgres -t -d $DBNAME -c 'SELECT relname from pg_class where relfilenode = $ONFILE;'"
+  OIDNAME=`psql -U postgres -t -d $DBNAME -c "SELECT relname from pg_class where relfilenode = $ONFILE;" 2>&1`
 }
 
 #Build read or write lists and flag if temp file is in list
@@ -57,14 +50,37 @@ function printlists {
 	#echo "DEBUG IOFILES $IOFILES RFIES $RFILES WFILES $WFILES"
 	for IOFILE in $IOFILES
 	do
-		ONFILE=`ls -l /proc/$MPPID/fd |grep " $IOFILE ->" | grep -oP '(?<=/)\d+(?=\.\d+$)'`
-                ONSUBFILE=`ls -l /proc/$MPPID/fd |grep " $IOFILE ->"|awk -F . '{print $NF}'`
-                FULLNAME=`ls -l /proc/$MPPID/fd |grep " $IOFILE ->"|awk -F"-> " {'print $2;'}`
-                MDIR=`ls -l /proc/$MPPID/fd |grep " $IOFILE ->" | grep -oP '(?<=\s)/.*(?=/[^/]+$)'`
-		MAXSUBFILE=`ls -alv $MDIR |grep $ONFILE|grep -v "_vm"|grep -v "_fsm"|tail -n1|awk -F . '{print $NF}'`
+                #echo $IOFILE
+		#echo "ls -l /proc/$MPPID/fd/$IOFILE | grep -oP '(?<=/)[^/.]+(?=(\.\d+)?$)'"
+	        ONFILE=`ls -l /proc/$MPPID/fd/$IOFILE | grep -oP '(?<=/)[^/.]+(?=(\.\d+)?$)'`
+                FULLNAME=`ls -l /proc/$MPPID/fd/$IOFILE | awk -F'-> ' {'print $2;'}`
+		#echo "O $ONFILE F $FULLNAME"
+		if [[ "$FULLNAME" =~ \. ]]; then
+                  ONSUBFILE=`ls -l /proc/$MPPID/fd/$IOFILE | awk -F . '{print $NF}'`
+                  #echo "ls -l /proc/$MPPID/fd/$IOFILE | grep -oP '(?<=\s)/[^ ]*(?=/[^/]+$)'"
+                  MDIR=`ls -l /proc/$MPPID/fd/$IOFILE | grep -oP '(?<=\s)/[^ ]*(?=/[^/]+$)'`
+                  #echo "ls -alv $MDIR |grep $ONFILE|grep -v '_vm'|grep -v '_fsm'|tail -n1|awk -F . '{print $NF}'"
+  		  MAXSUBFILE=`ls -alv $MDIR |grep $ONFILE|grep -v "_vm"|grep -v "_fsm"|tail -n1|awk -F . '{print $NF}'`
+                else 
+		  ONSUBFILE=1
+		  MAXSUBFILE=1
+                fi
+
 		isonlygig 
-		getfname $ONFILE
-                echo "Process I/O is in $OIDNAME at $ONSUBFILE out of $MAXSUBFILE at $FULLNAME"
+		#echo "ONFILE: $IOFILE $ONFILE"
+                if [[ "$FULLNAME" =~ pg_xlog ]] || [[ "$FULLNAME" =~ pg_wal ]]; then 
+                    OIDNAME=" <wal:$ONFILE>"
+                elif [[ "$FULLNAME" =~ pgsql_tmp ]]; then
+                    OIDNAME=" <tmp:$ONFILE>"
+		elif [[ "$FULLNAME" =~ _fsm ]]; then
+		    OIDNAME=" <fsm:$ONFILE>"
+                else 
+                    getfname $ONFILE
+		    if [[ -z $OIDNAME ]]; then
+                        OIDNAME=" <unknown:$ONFILE>"
+                    fi
+                fi
+                echo "Process I/O is in$OIDNAME at $ONSUBFILE out of $MAXSUBFILE at $FULLNAME"
 	done
 }
 
